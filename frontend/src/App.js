a8ad6642191f2e9c02dee2598ce33eb7fc5f452b
Sayan sen <sayan131416@gmail.com>
2024-02-18 18:01:52 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import AppRoutes from "./AppRoutes";
 import Header from "./components/header/Header";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header/>
+      <AppRoutes />
     </>
   );
 }
