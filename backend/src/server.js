c98c76e78ed1dc05d6f96b5e61608c2ebb0d3acb
Sayan sen <sayan131416@gmail.com>
2024-02-23 08:44:39 +0530

    publish

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import {fileURLToPath} from 'url';
+
 import express from 'express';
 import cors from 'cors';
 
@@ -9,7 +11,11 @@ import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 
 import { dbconnect } from './config/database.config.js';
+import { dirname } from 'path';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -24,6 +30,15 @@ app.use(cors({
 app.use('/api/foods',foodRouter);
 app.use('/api/users',userRouter);
 app.use('/api/orders',orderRouter);
+
+// const publicFolder = __dirname + '/' + 'public';
+const publicFolder = path.join(__dirname,'public');
+app.use(express.static(publicFolder));
+
+app.get('*',(req,res)=>{
+    const indexFilePath = path.join(publicFolder,'index.html');
+    res.sendFile(indexFilePath);
+})
 
 
 const PORT = 5000;
